6adccd429b43031453d3a1e77ae855accf8cf349
Maliha Hossain <malu.hoss@gmail.com>
2019-02-02 04:26:16 +0000

    Initial commit with all my work done in this folder for git module

---
+++ array-filtering.js
@@ -0,0 +1,9 @@
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+
+const evenNumbers = function (number) {
+	return number % 2 === 0;
+};
+
+const filtered = numbers.filter(evenNumbers);
+
+console.log(filtered);
